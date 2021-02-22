894e65834d8a2f50e26aa0446d919755db46b6a0
Sindri Már Kaldal Sigurjónsson <sindrimars@gmail.com>
2021-02-22 17:09:36 +0000

    initial commit

---
+++ src/Painting.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let url: string;
+    export let name: string;
+</script>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+    }
+    .painting {
+        width: 100px;
+        height: 100px;
+    }
+</style>
+
+<div class="wrapper">
+    <img alt={"a painting called " + name} src={url} class="painting"/>
+    <p class="title">{name}</p>
+</div>
