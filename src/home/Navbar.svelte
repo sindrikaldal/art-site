ca1885ea16b3892f0bb2a7409f73936e5b773842
Sindri Már Kaldal Sigurjónsson <sindrimars@gmail.com>
2021-02-24 11:18:43 +0000

    fixing hamburger animation

--- src/home/Navbar.svelte
+++ src/home/Navbar.svelte
@@ -52,21 +52,32 @@
         width: 35px;
         height: 5px;
         background-color: black;
-        margin: 6px 0 0 0;
+        transform-origin: center;
         transition: all 320ms ease;
-        transform-origin: left; 
-    }
-    .line1 {
-        transform: rotate(45deg) translate(0px, 1px);;
-        background-color: #F5DF4D;
         
     }
-    .line2 {
-        opacity: 0
+    .line:not(:last-child) {
+        margin: 0 0 8px 0;
     }
-    .line3 {
-        transform: rotate(-45deg) translate(-3px, 2px);;
+    .line1Out {
+        animation: topLineOut 720ms ease forwards;
         background-color: #F5DF4D;
+    }
+    .line1In {
+        animation: topLineIn 720ms ease forwards;
+    }
+    .line2In {
+        animation: fadeIn 720ms ease forwards;
+    }
+    .line2Out {
+        animation: fadeOut 720ms ease forwards;
+    }
+    .line3Out {
+        animation: bottomLineOut 720ms ease forwards;     
+        background-color: #F5DF4D;   
+    }
+    .line3In {
+        animation: bottomLineIn 720ms ease forwards;        
     }
     .hamburger {
         cursor: pointer;
@@ -78,15 +89,83 @@
         left: 15px;
         z-index: 99;
     }
+    @keyframes topLineOut {
+        0% {
+            transform: translateY(0) rotate(0deg);
+        }
+        50% {
+            transform: translateY(13px) rotate(0deg);
+        }
+        100% {
+            transform: translateY(13px) rotate(-45deg);
+        }
+    }
+    @keyframes topLineIn {
+        0% {
+            transform: translateY(13px) rotate(-45deg);
+        }
+        50% {
+            transform: translateY(13px) rotate(0deg);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+    @keyframes bottomLineOut {
+        0% {
+            transform: translateY(0) rotate(0deg);
+        }
+        50% {
+            transform: translateY(-13px) rotate(0deg);
+        }
+        100% {
+            transform: translateY(-13px) rotate(45deg);
+        }
+    }
+
+    @keyframes bottomLineIn {
+        0% {
+            transform: translateY(-13px) rotate(45deg);
+        }
+        50% {
+            transform: translateY(-13px) rotate(0deg);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+    
+    @keyframes fadeIn {
+        0% {
+            opacity: 0;
+        }
+        50% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+    @keyframes fadeOut {
+        0% {
+            opacity: 1;
+        }
+        25% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
      
 </style>
 
 <nav>
     <div class="navbar">
         <button on:click={onClick} class="hamburger">
-            <div class="line" class:line1={open}/>
-            <div class="line" class:line2={open}/>
-            <div class="line" class:line3={open}/>
+            <div class="line" class:line1Out={open} class:line1In={!open}/>
+            <div class="line" class:line2Out={open} class:line2In={!open}/>
+            <div class="line" class:line3Out={open} class:line3In={!open}/>
         </button>
     </div>
     <div class="menu" class:open={open} class:closed={!open}>
